aa5fa17a765967eed83dbb8cef3c418c150777bf
Scott McCracken <86990+smccracken@users.noreply.github.com>
2026-02-23 08:45:14 -0500

    Setup base config, linters, and CI workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import cssnano from "cssnano";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [cssnano],
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  prefetch: true,
+  site: "https://chocolatesculptress.com",
+  trailingSlash: "never",
+});
